f8e547ce936ecdac5a71b5643a0d8e8b37de991a
xuzekun02 <xuzekun02@meituan.com>
2025-06-04 16:16:09 +0800

    llama3_support

--- envs/tool_manager/__init__.py
+++ envs/tool_manager/__init__.py
@@ -1,11 +1,13 @@
+from .llama3_manager import Llama3Manager
 from .config_manager import ConfigManager
 from .qwen3_manager import QwenManager
 from .qwen2_5_manager import Qwen25Manager
 
-__all__ = ['ConfigManager', 'QwenManager']
+__all__ = ['ConfigManager', 'QwenManager','Qwen25Manager','Llama3Manager']
 
 TOOL_MANAGER_REGISTRY = {
     'config': ConfigManager,
     'qwen3': QwenManager,
-    'qwen2_5': Qwen25Manager
+    'qwen2_5': Qwen25Manager,
+    'llama3' : Llama3Manager,
 }
