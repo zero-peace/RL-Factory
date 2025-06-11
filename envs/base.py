800a7857eb232b2c50ca25f7a85a8221d01d868d
Jiajun Chai <33176111+James0618@users.noreply.github.com>
2025-06-11 14:15:09 +0800

    [update] minor updates

--- envs/base.py
+++ envs/base.py
@@ -24,12 +24,12 @@ class Env(ABC):
             # 分布式模式，保持原有逻辑
             if not tool_manager_name:
                 model_type = config.get('model_type', 'llama3')
-            if 'qwen3' in model_type:
-                tool_manager_name = 'qwen3'
-            elif 'qwen2' in model_type:
-                tool_manager_name = 'qwen2_5'
-            elif'llama' in model_type:
-                tool_manager_name = 'llama3'                     
+                if 'qwen3' in model_type:
+                    tool_manager_name = 'qwen3'
+                elif 'qwen2' in model_type:
+                    tool_manager_name = 'qwen2_5'
+                elif'llama' in model_type:
+                    tool_manager_name = 'llama3'                     
             self.tool_manager = TOOL_MANAGER_REGISTRY[tool_manager_name](verl_config=config)
         self.max_prompt_length = config.get('max_prompt_length', 2048)
         self.use_verify_tool = False
