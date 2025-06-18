6a59772238bd825df6c94219da9612b814440c66
Jiajun Chai <33176111+James0618@users.noreply.github.com>
2025-06-18 19:23:34 +0800

    [update] minor updates

--- envs/tool_manager/qwen2_5_manager.py
+++ envs/tool_manager/qwen2_5_manager.py
@@ -31,7 +31,6 @@ class Qwen25Manager(ToolManager):
         if isinstance(verl_config, dict):
             verl_config = OmegaConf.to_container(verl_config)
         super().__init__(verl_config)
-        self.functions = []
         self.generate_cfg = {
             'fncall_prompt_type': 'nous',
             'function_choice': 'auto',  # 注释掉这行
