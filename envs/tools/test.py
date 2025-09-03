04c17a8a590db387ee576dc7c5ab4bbe4224c71f
chengzihao02 <chengzihao02@meituan.com>
2025-09-03 12:10:25 +0800

    [feature] support Travelplanner

--- envs/tools/test.py
+++ envs/tools/test.py
@@ -4,4 +4,4 @@ from accommodation import query_accommodations
 
 # print(query_flights("Houston", "Wichita", "2022-04-04"))
 # print(query_attractions("Houston"))
-print(query_accommodations("Houston"))
+# print(query_accommodations("Houston"))
